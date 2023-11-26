5745e2fa1986920b9c0f70421d82848ced7aafe2
Bipin <bk3284488@gmail.com>
2023-11-26 12:15:37 +0530

    addd navbar and userlist

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Users from "./User/pages/Users";
 import NewPlace from "./Places/pages/NewPlace";
+import MainNavigation from "./Shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="*" element={<Users />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<Users />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
